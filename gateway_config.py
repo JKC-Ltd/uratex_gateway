ed097a8a0c7c6063cb2b4561cc914dc96ccd3b05
C H A R L E S <charleslei2017@gmail.com>
2026-01-09 03:32:07 +0800

    Update gateway_config.py

--- gateway_config.py
+++ gateway_config.py
@@ -1,45 +1,47 @@
 
 import db_connections
 import mysql.connector
-import time 
+import time
 from datetime import datetime
 import sys
 
 # DECLARING ID's
-gateway_id      = 2
-gateway_code    = "GAT-02"
+gateway_id = 1
+gateway_code = "GAT-01"
 
 
 def get_metter_ids():
-    meters_result   = []
-    local_conn      = db_connections.local_database()
-    query           = local_conn.cursor(dictionary=True)
-    #sql             = f"""SELECT sensors.id AS id, slave_address, sensor_reg_address, sensor_type_parameter FROM sensors 
-     #                       LEFT JOIN sensor_registers 
-      #                          ON sensors.sensor_register_id = sensor_registers.id
-       #                     LEFT JOIN sensor_types 
-        #                        ON  sensor_registers.sensor_type_id = sensor_types.id
-         #                           WHERE sensors.gateway_id = {gateway_id}"""
-                                    
-    sql             = f""" SELECT sensors.id AS id, slave_address, sensor_reg_address, sensor_type_parameter, sensor_models.id AS sensor_model_id FROM sensors
+    meters_result = []
+    local_conn = db_connections.local_database()
+    query = local_conn.cursor(dictionary=True)
+    # sql             = f"""SELECT sensors.id AS id, slave_address, sensor_reg_address, sensor_type_parameter FROM sensors
+    #                       LEFT JOIN sensor_registers
+    #                          ON sensors.sensor_register_id = sensor_registers.id
+    #                     LEFT JOIN sensor_types
+    #                        ON  sensor_registers.sensor_type_id = sensor_types.id
+    #                           WHERE sensors.gateway_id = {gateway_id}"""
+
+    sql = f""" SELECT sensors.id AS id, slave_address, sensor_reg_address, sensor_type_parameter, sensor_models.id AS sensor_model_id FROM sensors
                             LEFT JOIN sensor_models
                                 ON sensors.sensor_model_id = sensor_models.id
                             LEFT JOIN sensor_types
                                 ON sensor_models.sensor_type_id = sensor_types.id
                             WHERE sensors.gateway_id = {gateway_id}"""
     query.execute(sql)
-    
-    results     = query.fetchall()
+
+    results = query.fetchall()
 
     for row in results:
-        exploded_reg_address    = [int(value) for value in row['sensor_reg_address'].split(',')]
-        exploded_parameter      = [str(value) for value in row['sensor_type_parameter'].split(',')]
-        data      = {   'id':row['id'] , 
-                        'sensor_model_id': row['sensor_model_id'],
-                        'slave_address': row['slave_address'], 
-                        'register_address': exploded_reg_address, 
-                        'parameter': exploded_parameter
-                    }
+        exploded_reg_address = [
+            int(value) for value in row['sensor_reg_address'].split(',')]
+        exploded_parameter = [
+            str(value) for value in row['sensor_type_parameter'].split(',')]
+        data = {'id': row['id'],
+                'sensor_model_id': row['sensor_model_id'],
+                'slave_address': row['slave_address'],
+                'register_address': exploded_reg_address,
+                'parameter': exploded_parameter
+                }
         meters_result.append(data)
 
     query.close()
@@ -47,6 +49,6 @@ def get_metter_ids():
 
     return meters_result
 
-    
+
 # THIS CODE UNDER IS MORE LIKELY THE IMPLODE IN PHP
 # column_parameter = ", ".join(register_address["parameter"])
